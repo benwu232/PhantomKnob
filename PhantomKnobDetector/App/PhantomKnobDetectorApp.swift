d367cba2e76bf760fc003982f027b8eb2f32700f
benwu232 <benwu232@gmail.com>
2026-05-12 23:55:52 +1200

    chore: 初始化 Xcode 项目（任务 1/17）

---
+++ PhantomKnobDetector/App/PhantomKnobDetectorApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct PhantomKnobDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("PhantomKnobDetector - Placeholder")
+                .font(.largeTitle)
+                .padding()
+                .frame(width: 500, height: 400)
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
