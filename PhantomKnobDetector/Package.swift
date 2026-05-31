7871b19aef8d18089b0c477b66a6e687a98677ae
benwu232 <benwu232@gmail.com>
2026-05-31 18:32:10 +1200

    feat: add Package.swift to configure PhantomKnobDetector library and test targets

---
+++ PhantomKnobDetector/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "PhantomKnobDetector",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "PhantomKnobDetector",
+            targets: ["PhantomKnobDetector"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "PhantomKnobDetector",
+            path: ".",
+            sources: [
+                "App/PhantomKnobDetectorApp.swift",
+                "App/bundled-rules.json",
+                "Model/",
+                "ViewModel/",
+                "View/",
+                "Service/",
+                "Control/",
+                "Storage/",
+            ],
+            resources: [
+                .process("App/bundled-rules.json")
+            ]
+        ),
+        .testTarget(
+            name: "PhantomKnobDetectorTests",
+            dependencies: ["PhantomKnobDetector"],
+            path: "PhantomKnobDetectorTests"
+        )
+    ]
+)
