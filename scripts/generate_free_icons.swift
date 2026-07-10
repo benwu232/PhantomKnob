a0a3bf4ca6a68d97a0719a17a0b95b6ec47db5cd
benwu232 <benwu232@gmail.com>
2026-07-10 13:33:15 +1200

    style: reduce circular gap size from 1/4 (90 degrees) to 1/6 (60 degrees) in free status bar icons

--- scripts/generate_free_icons.swift
+++ scripts/generate_free_icons.swift
@@ -81,7 +81,7 @@ func drawStatusBarSlash(on image: NSImage) -> NSImage? {
     
     let center = CGPoint(x: S / 2.0, y: S / 2.0)
     let radius = S * 6.0 / 16.0
-    ctx.addArc(center: center, radius: radius, startAngle: -95.0 * .pi / 180.0, endAngle: 5.0 * .pi / 180.0, clockwise: false)
+    ctx.addArc(center: center, radius: radius, startAngle: -75.0 * .pi / 180.0, endAngle: -15.0 * .pi / 180.0, clockwise: false)
     ctx.strokePath()
     ctx.restoreGState()
     
